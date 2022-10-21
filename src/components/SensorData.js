57a2527a49dfbcdb0b0f14cf15d6ae80e6fc2b77
BrianCottrell <Brian.Cottrell.mobile@gmail.com>
2022-10-20 19:05:34 -0700

    Added map icons

--- src/components/SensorData.js
+++ src/components/SensorData.js
@@ -2,7 +2,14 @@ import React, {useState, useEffect, useRef} from 'react'
 import CloudCard from './CloudCard'
 import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
 import { useWindowSize } from '../hooks/WindowSize'
-import markerIconPng from "leaflet/dist/images/marker-icon.png"
+import AlertIcons_Generic from "../assets/AlertIcons_Generic.png"
+import MapIcon_Altitude from "../assets/MapIcon_Altitude.png"
+import MapIcon_Count from "../assets/MapIcon_Count.png"
+import MapIcon_Detected from "../assets/MapIcon_Detected.png"
+import MapIcon_Down from "../assets/MapIcon_Down.png"
+import MapIcon_Heatstroke from "../assets/MapIcon_Heatstroke.png"
+import MapIcon_Pulse from "../assets/MapIcon_Pulse.png"
+import MapIcon_Vehicles from "../assets/MapIcon_Vehicles.png"
 import { Icon } from 'leaflet'
 import { Spin } from 'antd'
 
@@ -266,16 +273,54 @@ function SensorData({}) {
     return <ul key={200}>{dataList(interval)}</ul>;
   });
 
+  const getMarkerIcon = marker => {
+    const sensorId = Number(marker["Sensor ID"]);
+      if (sensorId < 2000) {
+        return MapIcon_Vehicles;
+      } else if (sensorId < 3000) {
+        return MapIcon_Altitude;
+      } else if (sensorId < 4000) {
+        return MapIcon_Count;
+      } else if (sensorId < 5000) {
+        return MapIcon_Heatstroke;
+      } else if (sensorId < 6000) {
+        return MapIcon_Pulse;
+      } else if (sensorId < 7000) {
+        return MapIcon_Count;
+      } else if (sensorId < 8000) {
+        return MapIcon_Count;
+      } else if (sensorId < 9000) {
+        return MapIcon_Detected;
+      } else if (sensorId < 10000) {
+        return MapIcon_Pulse;
+      } else if (sensorId < 11000) {
+        return MapIcon_Detected;
+      } else if (sensorId < 12000) {
+        return MapIcon_Altitude;
+      } else if (sensorId < 13000) {
+        return MapIcon_Down;
+      }
+    return MapIcon_Altitude;
+  }
+
   const markerList = markers => {
     return markers.map(function(marker, index) {
       if (marker && marker.Lat && marker.Lon) {
         return (
           <Marker
             position={[marker.Lat, marker.Lon]}
-            icon={new Icon({iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41]})}
+            icon={new Icon({
+              iconUrl: getMarkerIcon(marker),
+              iconSize: [25, 41],
+              iconAnchor: [12, 41]
+            })}
           >
             <Popup>
-              {JSON.stringify(marker)}
+              {sensorListItem(
+                index,
+                "Marker",
+                [JSON.stringify(marker)]
+              )}
             </Popup>
           </Marker>
         );
