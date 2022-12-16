4ea435cca63f2fb56ef539960062a8b9c285a794
Chris Buonocore <chrisdistrict@gmail.com>
2022-12-15 20:16:12 -0500

    move into subcomponent

---
+++ src/components/VideoStream.js
@@ -0,0 +1,134 @@
+
+import { Empty, Spin, Button } from 'antd';
+import Boundingbox from './BoundingBox';
+import React, { useState, useEffect, useMemo } from 'react'
+import ReactPlayer from 'react-player'
+import { useInterval } from 'usehooks-ts'
+import { getAnalytic, getFrame } from '../api'
+import { getAnalyticEndpoint } from '../api/analytics'
+import { convertToArray, getBoundBoxes, getDataUrl, getReadableDateTime, getReadableError } from '../util'
+
+function VideoStream({ video, cameraId, onBoxClicked }) {
+    const [frame, setFrame] = useState()
+    const [first, setFirst] = useState(true)
+    const [analytics, setAnalytics] = useState()
+
+    async function nextFrame() {
+        try {
+            const { data } = await getFrame(cameraId)
+            setFrame(data)
+        } catch (e) {
+            console.error('error getting frame', e)
+        }
+    }
+
+
+    useInterval(nextFrame,
+        // Delay in milliseconds or null to stop it
+        (video && video.id) ? 1000 : null,
+    )
+
+
+    useEffect(() => {
+        // Clear.
+        setAnalytics()
+        setFrame()
+        setFirst(true)
+    }, [video])
+
+    const boxes = useMemo(() => {
+        const canvas = document.getElementById('box-canvas');
+        let height = 720
+        let width = 1280
+        if (canvas) {
+            width = canvas.getAttribute('width')
+            height = canvas.getAttribute('height')
+        }
+        const results = getBoundBoxes(analytics?.results || [], width, height)
+        console.log('boxes', results)
+        return results
+    }, [analytics])
+
+    const { image, endpoint } = analytics || {}
+
+
+    const imageUrl = getDataUrl(image)
+    const isDetection = endpoint === 'objectdetection' || endpoint === 'personattribute'
+
+    const loaded = video && frame;
+
+    async function fetchAnalytics(analyticName, firstFetch) {
+        const endpoint = getAnalyticEndpoint(analyticName)
+        console.log('get analytic', endpoint, video.id)
+        try {
+            const { data } = await getAnalytic(video.id, endpoint)
+            console.log('results', data)
+            if (typeof data?.results === 'string') {
+                data['results'] = JSON.parse(data.results)
+            }
+            setAnalytics({ ...data, analyticName, endpoint })
+        } catch (e) {
+            console.error('err', e)
+            let msg = e.toString()
+            if (msg.indexOf('503') !== 0) {
+                // Server outage / issue (should retry)
+                msg = 'Server temporarily unavailable. Please try again.'
+            }
+            alert('Error getting data: ' + msg.toString())
+        }
+
+        // TODO: hack to force rerender of bounding boxes.
+        if (firstFetch) {
+            setFirst(false)
+            fetchAnalytics(analyticName, false)
+        }
+    }
+
+    const onBoxSelected = (index) => {
+        const results = analytics?.results || []
+        console.log('box selected', index, results[index])
+        onBoxClicked(results[index])
+    }
+
+    return (
+        <div className='video-stream-content'>
+            {!video && <Empty description="No video stream active" />}
+            {video && !frame && <Spin size="large" />}
+            {/* https://github.com/CookPete/react-player */}
+            {loaded && <span className='standard-margin'>
+                {video.link && <ReactPlayer url={video.link} controls playing />}
+                {video.id && frame && <div>
+                    <p>Time: {getReadableDateTime(parseFloat(frame.timestamp) * 1000)}</p>
+                    <img className='analytics-image' alt="Image" src={getDataUrl(frame.image)} />
+                </div>}
+                <br />
+
+                <p>Click the buttons below to freeze frame.</p>
+                <div className='vertical-margin'>
+                    {/* Service button row */}
+                    {convertToArray(video.services).map((s, i) => {
+                        return <span key={i}><Button type="primary" onClick={() => fetchAnalytics(s, first)}>
+                            {s}
+                        </Button>&nbsp;</span>
+                    })}
+                </div>
+
+                {analytics && <span>
+                    <h3>{analytics.analyticName}</h3>
+                    {/* <img className='analytics-image' alt="Image" src={getDataUrl(analytics.image)} /> */}
+                    {imageUrl && <div style={{ height: 480, width: 640 }}>
+                        {analytics.results && <p>
+                            Results: {isDetection ? boxes?.length : analytics.results}
+                        </p>}
+                        <Boundingbox onSelected={(i) => onBoxSelected && onBoxSelected(i)} canvasId={'box-canvas'} image={imageUrl} boxes={boxes} />
+                    </div>}
+
+
+                </span>}
+            </span>}
+        </div>
+    )
+}
+
+
+export default VideoStream
--- src/components/VideoStreams.js
+++ src/components/VideoStreams.js
@@ -1,98 +1,23 @@
-import { Button, Card, Col, Empty, Input, Modal, Row, Spin, Table } from 'antd'
-import React, { useState, useEffect, useRef, useMemo } from 'react'
-import { FileUploader } from 'react-drag-drop-files'
-import ReactPlayer from 'react-player'
-import { getAnalytic, getCameras, getFrame } from '../api'
-import { getAnalyticEndpoint } from '../api/analytics'
-import { convertToArray, createObjectUrl, getBoundBoxes, getDataUrl, getReadableDateTime, getReadableError } from '../util'
+import { Button, Col, Input, Modal, Row, } from 'antd'
+import React, { useState, useEffect,  } from 'react'
 import CloudCard from './CloudCard'
 /*
 Example stream: https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/master.m3u8
 */
 
-import { useInterval } from 'usehooks-ts'
-import Boundingbox from './BoundingBox'
 import { useParams } from 'react-router-dom'
+import VideoStream from './VideoStream'
+import { getCameras } from '../api'
+import { getReadableError } from '../util'
 
 export default function VideoStreams() {
     const [videos, setVideos] = useState()
     const [video, setVideo] = useState()
     const [text, setText] = useState()
-    const [frame, setFrame] = useState()
-    const [first, setFirst] = useState(true)
     const [selected, setSelected] = useState() // Selected object.
-    const [analytics, setAnalytics] = useState()
 
     let { cameraId } = useParams()
     cameraId = parseInt(cameraId)
-
-    useEffect(() => {
-        // Clear.
-        setAnalytics()
-        setFrame()
-        setFirst(true)
-    }, [video])
-
-    async function nextFrame(cameraId) {
-        try {
-            const { data } = await getFrame(cameraId)
-            setFrame(data)
-        } catch (e) {
-            console.error('error getting frame', e)
-        }
-    }
-
-    useEffect(() => {
-        if (videos && cameraId) {
-            const v = videos.find(v => v.id === cameraId)
-            if (v) {
-                setVideo(v)
-            }
-        }
-    }, [videos, cameraId]);
-
-    useInterval(
-        () => {
-            // Your custom logic here
-            nextFrame(video?.id)
-        },
-        // Delay in milliseconds or null to stop it
-        (video && video.id) ? 1000 : null,
-    )
-
-    async function fetchAnalytics(analyticName, firstFetch) {
-        const endpoint = getAnalyticEndpoint(analyticName)
-        console.log('get analytic', endpoint, video.id)
-        try {
-            const { data } = await getAnalytic(video.id, endpoint)
-            console.log('results', data)
-            if (typeof data?.results === 'string') {
-                data['results'] = JSON.parse(data.results)
-            }
-            setAnalytics({ ...data, analyticName, endpoint })
-        } catch (e) {
-            console.error('err', e)
-            let msg = e.toString()
-            if (msg.indexOf('503') !== 0) {
-                // Server outage / issue (should retry)
-                msg = 'Server temporarily unavailable. Please try again.'
-            }
-            alert('Error getting data: ' + msg.toString())
-        }
-
-        // TODO: hack to force rerender of bounding boxes.
-        if (firstFetch) {
-            setFirst(false)
-            fetchAnalytics(analyticName, false)
-        }
-    }
-
-    const onBoxSelected = (index) => {
-        const results = analytics?.results || []
-        console.log('box selected', index, results[index])
-        setSelected(results[index])
-        // alert(results[index])
-    }
 
     async function cameras() {
         console.log('cameras')
@@ -108,29 +33,20 @@ export default function VideoStreams() {
     }
 
     useEffect(() => {
+        if (videos && cameraId) {
+            const v = videos.find(v => v.id === cameraId)
+            if (v) {
+                setVideo(v)
+            }
+        }
+    }, [videos, cameraId]);
+
+
+    useEffect(() => {
         cameras()
     }, [])
 
-    const boxes = useMemo(() => {
-        const canvas = document.getElementById('box-canvas');
-        let height = 720
-        let width = 1280
-        if (canvas) {
-            width = canvas.getAttribute('width')
-            height = canvas.getAttribute('height')
-        }
-        const results = getBoundBoxes(analytics?.results || [], width, height)
-        console.log('boxes', results)
-        return results
-    }, [analytics])
-
-    const { image, endpoint } = analytics || {}
-
-
-    const imageUrl = getDataUrl(image)
-    const isDetection = endpoint === 'objectdetection' || endpoint === 'personattribute'
-
-    const loaded = video && frame;
+    const onBoxClicked = (box) => setSelected(box)
 
     return (<div className='video-stream-content'>
         <Row>
@@ -157,43 +73,7 @@ export default function VideoStreams() {
             <Col span={1} />
             <Col span={16}>
                 <CloudCard minHeight={500} width="100%" title={`Selected Video${video ? `: ${video.name}` : ''}`}>
-                    <div className='video-stream-content'>
-                        {!video && <Empty description="No video stream active" />}
-                        {video && !frame && <Spin size="large" />}
-                        {/* https://github.com/CookPete/react-player */}
-                        {loaded && <span className='standard-margin'>
-                            {video.link && <ReactPlayer url={video.link} controls playing />}
-                            {video.id && frame && <div>
-                                <p>Time: {getReadableDateTime(parseFloat(frame.timestamp) * 1000)}</p>
-                                <img className='analytics-image' alt="Image" src={getDataUrl(frame.image)} />
-                            </div>}
-                            <br/>
-
-                            <p>Click the buttons below to freeze frame.</p>
-                            <div className='vertical-margin'>
-                            {/* Service button row */}
-                            {convertToArray(video.services).map((s, i) => {
-                                return <span key={i}><Button type="primary" onClick={() => fetchAnalytics(s, first)}>
-                                    {s}
-                                </Button>&nbsp;</span>
-                            })}
-                            </div>
-
-                            {analytics && <span>
-                                <h3>{analytics.analyticName}</h3>
-                                {/* <img className='analytics-image' alt="Image" src={getDataUrl(analytics.image)} /> */}
-                                {imageUrl && <div style={{ height: 480, width: 640 }}>
-                                    {analytics.results && isDetection && <p>
-                                        Results: {boxes?.length}
-                                    </p>}
-                                    <Boundingbox onSelected={(i) => onBoxSelected(i)} canvasId={'box-canvas'} image={imageUrl} boxes={boxes} />
-                                </div>}
-
-
-                            </span>}
-                        </span>}
-                    </div>
-
+                    <VideoStream video={video} cameraId={cameraId || video?.id} onBoxClicked={onBoxClicked} />
                 </CloudCard>
             </Col>
         </Row>
