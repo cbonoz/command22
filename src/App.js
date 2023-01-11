e62df5d9deabe8e147bbe8e0ff3bfc0309970948
Chris Buonocore <chrisdistrict@gmail.com>
2023-01-11 09:01:07 -0700

    logout

--- src/App.js
+++ src/App.js
@@ -89,6 +89,7 @@ export default function App() {
           label: (
             <a target="#" rel="noopener noreferrer" onClick={(e) => {
               e.preventDefault();
+              logout()
             }}>
               Logout
             </a>
